b4151bfe6b8386db679a1beb58c705fa5b5a61f2
Bill Kohler <bkohler4@gmail.com>
2018-04-10 12:10:37 -0400

    add subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'courses#index'
-  scope '/api' do
+  constraints subdomain: 'api' do
+    root 'courses#index'
     resources :students, :professors
     resources :courses, path: 'class'
   end
 end
+
+# test with POW - http://api.course-registration-api.test/
